deedb268e4d81b2f18ddf63394f314579c07179b
John <johnzkan@usc.edu>
2022-01-29 00:15:15 -0600

    fixing password

--- application/db.py
+++ application/db.py
@@ -5,9 +5,11 @@ from bson.json_util import dumps
 import ssl
 import certifi
 #from secret import PASSWORD
+import os
 
+password = os.environ.get('PASSWORD', None)
 
-CONNECTION_STRING = "mongodb+srv://johnkang54:"+PASSWORD+"@cluster0.dmkth.mongodb.net/flask_personal_website?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://johnkang54:"+password+"@cluster0.dmkth.mongodb.net/flask_personal_website?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING,tlsCAFile=certifi.where())
 db = client['flask_personal_website']
 print('db connected')
