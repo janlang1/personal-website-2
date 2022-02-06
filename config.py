269da789454252b7bd4d00e00fd5e99af69d3039
John <johnzkan@usc.edu>
2022-02-05 22:27:01 -0600

    new python runtime file

--- config.py
+++ config.py
@@ -2,9 +2,3 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xe2Z\xf2\xe4\x10\xf34<\x069t8Z\xd8w\x1e'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'johnkang03@gmail.com'
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
